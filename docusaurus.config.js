414f9f25545612d267e42b7ece20f6a35d20a4c7
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-05-24 12:50:14 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   tagline: 'Documentation make the world go round',
   favicon: 'img/favico.svg',
   url: 'https://your-docusaurus-site.example.com',
-  baseUrl: '/ITxD-docs/',
+  baseUrl: '/',
 
   organizationName: 'facebook',
   projectName: 'docusaurus',
